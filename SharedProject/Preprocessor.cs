59414808293bab038c7294316869edf1d30be696
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-01-13 12:54:35 +0300

    Use var

---
+++ SharedProject/Preprocessor.cs
@@ -0,0 +1,19 @@
+﻿namespace net6_sharp10_lib
+{
+    public class Preprocessor
+    {
+#if NET6_0
+        public void Test()
+        {
+            string s = "";
+        }
+#else
+        public void Test()
+        {
+            // use var
+            int t = 1;
+
+        }
+#endif
+    }
+}
