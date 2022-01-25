06e62e1999fe165eb5d7ce1ea54fe00cf8e3efaf
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-01-25 11:00:53 +0300

    Add MVC.

---
+++ SharedProject/Shared4.cs
@@ -0,0 +1,10 @@
+﻿namespace net6_sharp10_lib
+{
+    public class Shared4
+    {
+        public string PublicMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
