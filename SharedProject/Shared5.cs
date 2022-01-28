b0271334fa0367044c8bbf6ce5390e2f4694b3fd
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-01-28 11:19:11 +0300

    Add "Use type keyword".

---
+++ SharedProject/Shared5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace net6_sharp10_lib
+{
+
+    public class Shared5
+    {
+        public void Test(Shared4.InnerClass innerClass)
+        {
+            if (innerClass.Name == String.Empty)
+            {
+                
+            }
+            if (innerClass.Name == String.Empty)
+            {
+                
+            }
+            if (innerClass.Name == String.Empty)
+            {
+                
+            }
+        }
+
+        public string NewName { get; set; }
+    }
+}
---
+++ net6_csharp10_lib/UseShared5.cs
@@ -0,0 +1,17 @@
+﻿using net6_sharp10_lib;
+
+namespace net6_csharp10_lib;
+
+public class UseShared5
+{
+    private void Test()
+    {
+        var useShared5 = new Shared5();
+        useShared5.NewName = "";
+
+        if (useShared5.NewName == String.Empty)
+        {
+            
+        }
+    }
+}
